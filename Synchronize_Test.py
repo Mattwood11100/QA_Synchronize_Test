de19f1db0fd8aa10c9c977f8f3eb531dfec68d52
Mattwood11100 <memattwood@gmail.com>
2023-03-21 17:15:45 +0000

    Creating initial files and folders

---
+++ Synchronize_Test.py
@@ -0,0 +1,9 @@
+import os
+import argparse
+
+# parser = argparse.ArgumentParser(description="This program will synchronize the contents of one folder to another folder",
+#                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+
+print(os.getcwd())
+print(os.listdir())
