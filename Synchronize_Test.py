d5ca1d05e80b260e52a3221ef90cee314a00d762
Mattwood11100 <memattwood@gmail.com>
2023-03-28 11:28:51 +0100

    Complete synchronization script

--- Synchronize_Test.py
+++ Synchronize_Test.py
@@ -1,89 +1,150 @@
+# Imported libraries
 import os
 import filecmp
 import shutil
-import pathlib
+import stat
 import argparse
+from datetime import datetime
+from apscheduler.schedulers.background import BlockingScheduler
 
-# parser = argparse.ArgumentParser(description="This program will synchronize the contents of one folder to another folder",
-#                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+# Command line arguments
+parser = argparse.ArgumentParser(description='A script to perform a one way sync between a source folder and a destination folder.')
+parser.add_argument("Source", help="File path of the source folder")
+parser.add_argument("Destination", help="File path of the destination folder")
+parser.add_argument("Sync_Interval", help="Number of minutes between consecutive synchronizations ")
+parser.add_argument("Logfile", help="File path of the log file")
+args = parser.parse_args()
 
+# Assigning the command line arguments to relevant variables
+sourceDir = args.Source
+destinationDir = args.Destination
+syncInterval = args.Sync_Interval
+logFileDir = args.Logfile
 
-CWD = os.getcwd()
+# Checking if the paths provides exist
+try:
+    if not os.path.exists(sourceDir):
+        raise ValueError(f"The provided source path does not exist.")
+except ValueError as error:
+    print(repr(error))
 
-sourceDir = os.path.join(CWD, "Source")
-destinationDir = os.path.join(CWD, "Destination")
+try:
+    if not os.path.exists(destinationDir):
+        raise ValueError(f"The provided destination path does not exist.")
+except ValueError as error:
+    print(repr(error))
 
-# sourceDir2 = pathlib.Path(os.path.join(CWD, "Source"))
-# sourceDir2 = pathlib.Path("Source")
-# destinationDir2 = pathlib.Path(os.path.join(CWD, "Destination"))
-
-
-# print(f"CWD:\t{CWD}\n"
-#       f"Source:\t{sourceDir}\n"
-#       f"Destination:\t{destinationDir}")
-#
-# print(f"Contents of Source:\t{os.listdir(sourceDir)}\n")
-# print(f"Recursive Contents of Source:\t{list(sourceDir2.rglob('*'))[1]}\n")
-# print(f"Recursive Contents of Destination:\t{list(destinationDir2.rglob('*'))}\n")
-#
-comparison = filecmp.dircmp(sourceDir, destinationDir)
-#
-print(f"Common:\t\t\t\t{comparison.common}\n"
-      f"Source only:\t\t{comparison.left_only}\n"
-      f"Destination only:\t{comparison.right_only}\n")
-# print("Done")
-
-
-# Compare the contents of the two folders
-# Comparing the first layer of the two directories
-comp = filecmp.dircmp(sourceDir, destinationDir)
-
-# If the destination directory is empty, deep copy all the contents of the source folder to the destination folder
-if len(comp.common) == 0 and len(comp.right_only) == 0:
-    shutil.copytree(sourceDir, destinationDir, dirs_exist_ok=True)
-    print(f"Copied contents of source to destination\n")
-
-# Deep copy the file only in the source directory to the destination directory
-elif len(comp.left_only) != 0:
-    for file in comp.left_only:
-        isFile = os.path.isfile(os.path.join(sourceDir, file))
-        isDirc = os.path.isdir(os.path.join(sourceDir, file))
-        # print(f"is File:\t{isFile}\n"
-        #       f"is Dirc:\t{isDirc}\n")
-        if isFile:
-            shutil.copy2(os.path.join(sourceDir, file), destinationDir)
-            print(f"Copied the single file\n")
-        elif isDirc:
-            shutil.copytree(os.path.join(sourceDir, file), os.path.join(destinationDir, file))
-            print(f"Copied the single folder\n")
+try:
+    if not os.path.exists(logFileDir):
+        raise ValueError(f"The provided log file path does not exist.")
+except ValueError as error:
+    print(repr(error))
 
 
-# TODO: Remove the files that are only in the destination directory
-elif len(comp.right_only) != 0:
-    for file in comp.right_only:
-        isFile = os.path.isfile(os.path.join(destinationDir, file))
-        isDirc = os.path.isdir(os.path.join(destinationDir, file))
-        # print(f"is File:\t{isFile}\n"
-        #       f"is Dirc:\t{isDirc}\n")
-        if isFile:
-            os.remove(os.path.join(destinationDir, file))
-            print(f"Removed the single file\n")
-        # elif isDirc:
-        #     shutil.rmtree(os.path.join(destinationDir, file))
-        #     print(f"Removed the single folder\n")
+def errorHandler(function, path, executeInformation):
+    os.chmod(path, stat.S_IWRITE)
+    function(path)
 
 
-# TODO: Compare the common files and determine if the file is a file or directory, if there are differences between the files then
-#       copy the source file to the destination directory
+def syncFolder(sourceDir, destinationDir, logFile):
+    # Compare the contents of the source and destination folders
+    comp = filecmp.dircmp(sourceDir, destinationDir)
+    syncCheck = False
+    with open(logFileDir, "a") as log:
+
+        # Printing a line of * so that the text file and console are easier to read
+        log.write(f"*" * 80 + "\n")
+        print(f"*" * 80 + "\n")
+
+        # If the destination directory is empty, deep copy all the contents of the source folder to the destination folder
+        if not comp.common and not comp.right_only:
+            shutil.copytree(sourceDir, destinationDir, dirs_exist_ok=True, symlinks=True)
+            syncCheck = True
+            for file in comp.left_only:
+                log.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was created.\n")
+                print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was created.\n")
+
+        else:
+
+            # Deep copy the file only in the source directory to the destination directory
+            if comp.left_only:
+                for file in comp.left_only:
+                    # Checking if the element in the list is a file or directory
+                    isFile = os.path.isfile(os.path.join(sourceDir, file))
+                    isDirc = os.path.isdir(os.path.join(sourceDir, file))
+                    syncCheck = True
+
+                    if isFile:
+                        shutil.copy2(os.path.join(sourceDir, file), destinationDir)
+                        log.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was copied.\n")
+                        print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was copied.\n")
+
+                    elif isDirc:
+                        shutil.copytree(os.path.join(sourceDir, file), os.path.join(destinationDir, file))
+                        log.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was copied.\n")
+                        print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was copied.\n")
+
+            # Remove the files that are only in the destination directory
+            if comp.right_only:
+                for file in comp.right_only:
+                    # Checking if the element in the list is a file or directory
+                    isFile = os.path.isfile(os.path.join(destinationDir, file))
+                    isDirc = os.path.isdir(os.path.join(destinationDir, file))
+                    syncCheck = True
+
+                    if isFile:
+                        os.remove(os.path.join(destinationDir, file))
+                        log.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was removed.\n")
+                        print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was removed.\n")
+
+                    elif isDirc:
+                        shutil.rmtree(os.path.join(destinationDir, file), onerror=errorHandler)
+                        log.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was removed.\n")
+                        print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was removed.\n")
+
+            # Compare the common files and determine if the file is a file or directory, if there are differences between the files
+            # then copy the source file to the destination directory
+            if comp.common:
+                for file in comp.common:
+                    # Checking if the element in the list is a file or directory
+                    isFile = os.path.isfile(os.path.join(destinationDir, file))
+                    isDirc = os.path.isdir(os.path.join(destinationDir, file))
+
+                    if isFile:
+                        fileMatch, fileDifferent, fileErrors = filecmp.cmpfiles(sourceDir, destinationDir, common=[file])
+
+                        if fileDifferent:
+                            syncCheck = True
+                            shutil.copy2(os.path.join(sourceDir, file), destinationDir)
+                            log.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was replaced.\n")
+                            print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was replaced.\n")
+
+                    elif isDirc:
+                        dirDifferent = filecmp.dircmp(os.path.join(sourceDir, file), os.path.join(destinationDir, file)).diff_files
+
+                        if dirDifferent:
+                            syncCheck = True
+                            shutil.rmtree(os.path.join(destinationDir, file), onerror=errorHandler)
+                            shutil.copytree(os.path.join(sourceDir, file), os.path.join(destinationDir, file))
+                            log.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was replaced.\n")
+                            print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}-->\t{destinationDir}\{file} was replaced.\n")
+
+        if not syncCheck:
+            log.write(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} --> No changes have occurred\n")
+            print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} --> No changes have occurred\n")
+
+        syncCheck = False
+
+        # Printing a line of = so that the text file and console are easier to read
+        log.write(f"=" * 80 + "\n\n")
+        print(f"=" * 80 + "\n\n")
 
 
-# TODO: store the relevant action in a log file as well as the console
+# Creates a background scheduler
+syncScheduler = BlockingScheduler()
 
+syncScheduler.add_job(func=syncFolder, trigger='interval', args=[sourceDir, destinationDir, logFileDir], minutes=int(syncInterval))
+# syncScheduler.add_job(func=syncFolder, trigger='interval', args=[sourceDir, destinationDir, logFileDir], seconds=)
 
-# TODO: Add the command line arguments
-
-
-# TODO: Make the synchronization periodic
-
-
-# TODO: Add contents to the readme file
+# Starts the Scheduled jobs
+syncScheduler.start()
